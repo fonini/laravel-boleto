4d4e6cbbd17f969f1df150f4b83be37beb03a3b7
Jonnas Fonini <jonnas@sbsistemas.com.br>
2020-03-11 15:18:46 -0300

    Add Unicred

--- src/Boleto/Banco/Sicredi.php
+++ src/Boleto/Banco/Sicredi.php
@@ -19,7 +19,7 @@ class Sicredi extends AbstractBoleto implements BoletoContract
      *
      * @var string
      */
-    protected $localPagamento = 'Pagável preferencialmente nas cooperativas de crédito do sicredi';
+    protected $localPagamento = 'Pagável preferencialmente nas cooperativas de crédito do Sicredi';
     /**
      * Código do banco
      *
@@ -175,6 +175,7 @@ class Sicredi extends AbstractBoleto implements BoletoContract
             . CalculoDV::sicrediNossoNumero($this->getAgencia(), $this->getPosto(), $this->getConta(), $ano, $byte, $numero_boleto);
         return $nossoNumero;
     }
+
     /**
      * Retorna o Nosso Número.
      *
